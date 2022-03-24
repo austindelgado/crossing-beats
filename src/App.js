a047ef31675cec2493eed46ee665cd2501688f24
Austin <austinrdelgado@gmail.com>
2022-03-24 16:24:53 -0400

    Moved buttons to musicPlayer component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import {Howl, Howler} from 'howler';
 import Clock from './components/clock';
+import MusicPlayer from './components/musicPlayer';
 
 function App() {
-
-  var sound = new Howl({
-    src: ['/songs/scruffy6AM.mp3'],
-    loop: true,
-    onplayerror: function() {
-      console.log('error');
-    }
-  });
-  
   return (
     <div className="App">
       <Clock></Clock>
-      <button onClick={() => sound.play()}>
-        PLAY
-      </button>
+      <MusicPlayer></MusicPlayer>
     </div>
   );
 }
